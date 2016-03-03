ae302d133505b9b1c849066065832810635337c3
peterk99 <peterk.ruby@gmail.com>
2016-03-03 00:43:17 -0500

    Updated the forms
    Updated the forms as per the date field selected in pets
    Updated the seed data, removed the syntax and typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Doctor.create([{name:"Owner", address:"12, Street Name", city: "Fargo",state:"North Dakota", zip:12345, certified_by:"MIT", experience:12},{name:"Bob", address:"12, Street Name", city: "Fargo",state:"North Dakota", zip:12345, certified_by:"MIT", experience:12},{name:"Susan", address:"12, Street Name", city: "Fargo",state:"North Dakota", zip:12346, certified_by:"MIT", experience:08}])
+Doctor.create([{name:"Owner", address:"12, Street Name", city: "Fargo",state:"North Dakota", zip:12345, certified_by:"MIT", experience:12},{name:"Bob", address:"12, Street Name", city: "Fargo",state:"North Dakota", zip:12345, certified_by:"MIT", experience:12},{name:"Susan", address:"12, Street Name", city: "Fargo",state:"North Dakota", zip:12346, certified_by:"MIT", experience:18}])
 
-Pet.create([{name:"Scooby" breed:"Great Dane" type:"dog" age:12 weight:45 last_appointment:2015-12-12},{name:"Garfield" breed:"Exotic" type:"cat" age:12 weight:45 last_appointment:2015-12-12}])
+Pet.create([{name:"Scooby", breed:"Great Dane", pet_type:"dog", age:12, weight:45, last_appointment:2015/12/12},{name:"Garfield", breed:"Exotic", pet_type:"cat", age:12, weight:45, last_appointment:2015/12/12}])
